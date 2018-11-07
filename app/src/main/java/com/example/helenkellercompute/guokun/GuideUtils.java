a15bde33420036a347e9806d8b6feb3a402a821d
gkkunguo <gkkunguo@163.com>
2018-11-07 20:23:16 +0800

    1

---
+++ app/src/main/java/com/example/helenkellercompute/guokun/GuideUtils.java
@@ -0,0 +1,87 @@
+package com.example.helenkellercompute.guokun;
+
+import com.example.helenkellercompute.guokun.adapter.AdapterDemo;
+import com.example.helenkellercompute.guokun.alertdialog.AlertDialogDemo;
+import com.example.helenkellercompute.guokun.dataOrOracle.OracleDemo;
+import com.example.helenkellercompute.guokun.design.DesignDemosActivity;
+import com.example.helenkellercompute.guokun.gridview.GridViewDemo;
+import com.example.helenkellercompute.guokun.handlerOrMessage.HandlerOrMessage1;
+import com.example.helenkellercompute.guokun.intent.IntenDemo;
+import com.example.helenkellercompute.guokun.json.JsonDemo;
+import com.example.helenkellercompute.guokun.layoutObject.LayoutObjectDemo;
+import com.example.helenkellercompute.guokun.musicandvideo.MusicAndVideoDemo;
+import com.example.helenkellercompute.guokun.spinner.SpinnerDemo;
+import com.example.helenkellercompute.guokun.thread.handlerthread.ThreadActivity;
+import com.example.helenkellercompute.guokun.xml.XmlDemo;
+
+/**
+ * Created by Helen keller compute on 2018/11/7.
+ */
+
+public class GuideUtils {
+    //触屏
+    private OnTouch onTouch;
+
+    //Service
+    private ServiceDemo serviceDemo;
+
+    //广播
+    private BroadcastReceiverDemo broadcastReceiverDemo;
+
+    //布局文件
+    private LayoutObjectDemo layoutObjectDemo;
+
+    //Handler
+    private HandlerOrMessage1 handlerOrMessage1;
+
+    //解析文件
+    private XmlDemo xmlDemo;
+    private JsonDemo jsonDemo;
+
+
+    //设计模式
+    private DesignDemosActivity designDemosActivity;
+
+    //音频与视频与拍照
+    private MusicAndVideoDemo musicAndVideoDemo;
+
+    //数据存储
+    private OracleDemo oracleDemo;
+
+    //Thread
+    private ThreadActivity threadActivity;
+
+    //gridView
+    private GridViewDemo gridViewDemo;
+
+    //IntenDemo
+    private IntenDemo intenDemo;
+
+    //AlertDialog解析
+    private AlertDialogDemo alertDialogDemo;
+
+    //适配器
+    private AdapterDemo adapterDemo;
+
+    //级联
+    private SpinnerDemo spinnerDemo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
