411efa456837045e21c71124a692b9cfd7b51fae
guokun <gkkunguo@163.com>
2020-01-01 23:05:30 +0800

    AIDL

---
+++ app/src/main/java/com/example/helenkellercompute/guokun/androidview/SwipeRefreshLayout_banner/SwipeLayoutBannerActivity.java
@@ -0,0 +1,85 @@
+package com.example.helenkellercompute.guokun.androidview.SwipeRefreshLayout_banner;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.bumptech.glide.Glide;
+import com.example.helenkellercompute.guokun.R;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.listener.OnBannerListener;
+import com.youth.banner.loader.ImageLoader;
+import com.youth.banner.loader.ImageLoaderInterface;
+
+import java.util.ArrayList;
+
+public class SwipeLayoutBannerActivity extends AppCompatActivity implements OnBannerListener {
+    private SwipeRefreshLayout swipeRefreshLayout;
+//    private ListView listView;
+    private Banner banner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_layout_banner);
+
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshId);
+//        listView = findViewById(R.id.listviewId);
+//
+//        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+//            @Override
+//            public void onRefresh() {
+//
+//            }
+//        });
+
+        banner = findViewById(R.id.bannerId);
+        ArrayList<String> list_path = new ArrayList<>();
+        ArrayList<String> list_title = new ArrayList<>();
+
+        list_path.add("http://ww4.sinaimg.cn/large/006uZZy8jw1faic21363tj30ci08ct96.jpg");
+        list_path.add("http://ww4.sinaimg.cn/large/006uZZy8jw1faic259ohaj30ci08c74r.jpg");
+        list_path.add("http://ww4.sinaimg.cn/large/006uZZy8jw1faic2b16zuj30ci08cwf4.jpg");
+        list_path.add("http://ww4.sinaimg.cn/large/006uZZy8jw1faic2e7vsaj30ci08cglz.jpg");
+        list_title.add("好好学习");
+        list_title.add("天天向上");
+        list_title.add("热爱劳动");
+        list_title.add("不搞对象");
+        //设置内置样式，共有六种可以点入方法内逐一体验使用。
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE);
+        //设置图片加载器，图片加载器在下方
+        banner.setImageLoader(new ImageLoader() {
+            @Override
+            public void displayImage(Context context, Object path, ImageView imageView) {
+                Glide.with(context).load((String) path).into(imageView);
+            }
+        });
+        //设置图片网址或地址的集合
+        banner.setImages(list_path);
+        //设置轮播的动画效果，内含多种特效，可点入方法内查找后内逐一体验
+        banner.setBannerAnimation(Transformer.Default);
+        //设置轮播图的标题集合
+        banner.setBannerTitles(list_title);
+        //设置轮播间隔时间
+        banner.setDelayTime(1000);
+        //设置是否为自动轮播，默认是“是”。
+        banner.isAutoPlay(true);
+        //设置指示器的位置，小点点，左中右。
+        banner.setIndicatorGravity(BannerConfig.CENTER)
+                //以上内容都可写成链式布局，这是轮播图的监听。比较重要。方法在下面。
+                .setOnBannerListener(SwipeLayoutBannerActivity.this)
+                //必须最后调用的方法，启动轮播图。
+                .start();
+    }
+    //轮播图的监听方法
+    @Override
+    public void OnBannerClick(int position) {
+        Log.i("tag", "你点了第"+position+"张轮播图");
+    }
+}
